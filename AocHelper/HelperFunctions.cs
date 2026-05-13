00462f51f3ccfa3dc11046d4c35bf17d73f5bcfc
Simon Da Vall <simon.davall@gmail.com>
2026-05-13 09:16:01 +0100

    Day 01 and project setup

---
+++ AocHelper/HelperFunctions.cs
@@ -0,0 +1,246 @@
+using System.Text;
+using System.Numerics;
+
+namespace AocHelper;
+
+public static class Helper
+{
+  public static List<T> List<T>(List<T> list)
+  {
+    return list;
+  }
+
+  public static IEnumerable<int> Range(int n)
+  {
+    return Enumerable.Range(0, n);
+  }
+
+  public static IEnumerable<int> Range(int start, int n)
+  {
+    return Enumerable.Range(start, n);
+  }
+
+  public static long[] Range(long start, long n)
+  {
+    var range = new long[n];
+    for (long i = 0; i < n; i++)
+      range[i] = i + start;
+    return range;
+  }
+
+  public static int ToInt(this string str)
+  {
+    if (int.TryParse(str, out var value))
+      return value;
+
+    throw new InvalidCastException($"Not a valid integer: {str}");
+  }
+
+  public static long ToLong(this string str)
+  {
+    if (long.TryParse(str, out var value))
+      return value;
+
+    throw new InvalidCastException($"Not a valid integer: {str}");
+  }
+
+  public static int[] ToIntArray(this string[] array)
+  {
+    var intArray = new int[array.Length];
+    for (var i = 0; i < array.Length; i++) {
+      intArray[i] = array[i].ToInt();
+    }
+
+    return intArray;
+  }
+
+  public static int[] ToIntArray(this Span<string> array)
+  {
+    var intArray = new int[array.Length];
+    for (var i = 0; i < array.Length; i++) {
+      intArray[i] = array[i].ToInt();
+    }
+
+    return intArray;
+  }
+
+  public static long[] ToLongArray(this string[] array)
+  {
+    var longArray = new long[array.Length];
+    for (var i = 0; i < array.Length; i++) {
+      longArray[i] = array[i].ToLong();
+    }
+
+    return longArray;
+  }
+
+  public static char[][] ToCharArray(this string[] array)
+  {
+    var charArr = new char[array.Length][];
+    for (var i = 0; i < array.Length; i++)
+      charArr[i] = array[i].ToCharArray();
+
+    return charArr;
+  }
+
+  public static int[][] To2DIntArray(this string[] array)
+  {
+    var intArr = new int[array.Length][];
+    for (var i = 0; i < array.Length; i++) {
+      intArr[i] = new int[array[i].Length];
+      for (var j = 0; j < array[i].Length; j++) {
+        intArr[i][j] = array[i][j] - '0';
+      }
+    }
+
+    return intArr;
+  }
+
+  public static T[] CreateArray<T>(int size, T defaultValue)
+  {
+    T[] array = new T[size];
+    for (var i = 0; i < size; i++) array[i] = defaultValue;
+    return array;
+  }
+
+  public static (T first, T second) ToTuplePair<T>(this T[] array)
+  {
+    return array.Length switch {
+      > 2 => throw new ArgumentException(
+          $" Too many array members.{array.Length} This method requires an array of length 2."),
+      < 2 => throw new ArgumentException(
+          $" Too few array members.{array.Length} This method requires an array of length 2."),
+      _ => (array[0], array[1])
+    };
+  }
+
+  public static (int first, int second) ToIntTuplePair(this string[] array)
+  {
+    if (array.Length > 2)
+      throw new ArgumentException(
+          $" Too many array members.{array.Length} This method requires an array of length 2.");
+    if (array.Length < 2)
+      throw new ArgumentException(
+          $" Too few array members.{array.Length} This method requires an array of length 2.");
+
+    return (array[0].ToInt(), array[1].ToInt());
+  }
+
+  public static (long first, long second) ToLongTuplePair(this string[] array)
+  {
+    if (array.Length > 2)
+      throw new ArgumentException(
+          $" Too many array members.{array.Length} This method requires an array of length 2.");
+    if (array.Length < 2)
+      throw new ArgumentException(
+          $" Too few array members.{array.Length} This method requires an array of length 2.");
+
+    return (array[0].ToLong(), array[1].ToLong());
+  }
+
+  public static (int first, int second, int third) ToIntTupleTriple(this string[] array)
+  {
+    return array.Length switch {
+      > 3 => throw new ArgumentException(
+          $" Too many array members.{array.Length} This method requires an array of length 3."),
+      < 3 => throw new ArgumentException(
+          $" Too few array members.{array.Length} This method requires an array of length 3."),
+      _ => (array[0].ToInt(), array[1].ToInt(), array[2].ToInt())
+    };
+  }
+
+  public static (long first, long second, long third) ToLongTupleTriple(this string[] array)
+  {
+    return array.Length switch {
+      > 3 => throw new ArgumentException(
+          $" Too many array members.{array.Length} This method requires an array of length 3."),
+      < 3 => throw new ArgumentException(
+          $" Too few array members.{array.Length} This method requires an array of length 3."),
+      _ => (array[0].ToLong(), array[1].ToLong(), array[2].ToLong())
+    };
+  }
+
+  public static List<T> ToSortedList<T>(this HashSet<T> set)
+  {
+    var list = set.ToList();
+    list.Sort();
+    return list;
+  }
+
+  public static List<T> Sorted<T>(this List<T> list)
+  {
+    list.Sort();
+    return list;
+  }
+
+  public static List<T> SortedDesc<T>(this List<T> list)
+  {
+    return list.OrderDescending().ToList();
+  }
+
+  public static string Print<T>(this T[] arr, int max = 10)
+  {
+    if (arr.Length > max) {
+      return $"[{string.Join(", ", arr[..max])} ... ]";
+    } else {
+      return $"[{string.Join(", ", arr)}]";
+    }
+  }
+
+  public static string PrintPlain<T>(this T[] arr, int max = 10)
+  {
+    if (arr.Length > max) {
+      return $"{string.Join("", arr[..max])} ... ";
+    } else {
+      return $"{string.Join("", arr)}";
+    }
+  }
+
+  public static string Print<TKey, TValue>(this Dictionary<TKey, TValue> dict, int max = 10) where TKey : notnull
+  {
+    var str = new StringBuilder();
+    foreach (var (k, v) in dict) {
+      str.Append($"[{k}:{v}], ");
+      if (--max == 0) {
+        str.Append($"... max {max} shown");
+        break;
+      }
+    }
+    Console.WriteLine();
+    return str.ToString();
+  }
+
+  public static void VarDump<T>(T[] arr)
+  {
+    Console.Write("[");
+    for (var i = 0; i < arr.Length; i++) {
+      Console.Write($"{arr[i]}, ");
+    }
+    Console.Write("\b\b]\n");
+    Console.WriteLine();
+  }
+
+  public static void VarDump<T>(T[][] arr) where T : INumber<T>
+  {
+    var len = arr.Length;
+    int idxLen = len.ToString().Length;
+    for (var i = 0; i < len; i++) {
+      PaddedString("[", i.ToString(), '0', idxLen, "] = [");
+      for (var j = 0; j < arr[i].Length; j++) {
+        if (arr[i][j] < T.Zero)
+          Console.Write($"\u001b[31m{T.Abs(arr[i][j])}\u001b[0m, ");
+        else
+          Console.Write($"\u001b[32m{arr[i][j]}\u001b[0m, ");
+      }
+      Console.Write("\b\b]\n");
+    }
+    Console.WriteLine();
+  }
+
+  private static void PaddedString(string prefix, string value, char format, int padding, string postfix)
+  {
+    Console.Write(prefix);
+    Console.Write(string.Format("{0," + padding + "}", value));
+    Console.Write(postfix);
+  }
+}
---
+++ Day01/Solution.cs
@@ -0,0 +1,41 @@
+﻿namespace Day01;
+
+internal static partial class Program
+{
+    private const string Title = "\n## Day 1: Not Quite Lisp ##";
+    private const string AdventOfCode = "https://adventofcode.com/2015/day/1";
+    private const long ExpectedPartOne = 74;
+    private const long ExpectedPartTwo = 1795;
+
+    private static long PartOne(string data)
+    {
+        long floor = 0;
+
+        foreach (char c in data)
+        {
+            floor += Next(c, floor);
+        }
+
+        return floor;
+    }
+
+    private static long PartTwo(string data)
+    {
+        long floor = 0;
+        int i;
+
+        for (i = 0; i < data.Length; i++)
+        {
+            floor += Next(data[i], floor);
+            if (floor < 0)
+                break;
+        }
+
+        return i + 1;
+    }
+
+    private static long Next(char c, long floor)
+    {
+        return c == '(' ? 1 : -1;
+    }
+}
