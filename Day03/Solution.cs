0b4a4dacf177d77865dea066e937abd2ab1e3d7a
Simon Da Vall <simon.davall@gmail.com>
2026-05-24 09:39:41 +0100

    Day03

---
+++ Day03/Solution.cs
@@ -0,0 +1,57 @@
+﻿namespace Day03;
+
+internal static partial class Program {
+  private const string Title = "\n## Day 3: Perfectly Spherical Houses in a Vacuum ##";
+  private const string AdventOfCode = "https://adventofcode.com/2015/day/3";
+  private const long ExpectedPartOne = 0;
+  private const long ExpectedPartTwo = 0;
+
+
+  private static readonly Dictionary<char, (int, int)> Directions = new()
+  {
+    {'^', (0, -1)},
+    {'v', (0, 1)},
+    {'<', (-1, 0)},
+    {'>', (1, 0)},
+  };
+  
+  private static long PartOne(string data) {
+    (int x, int y) santa = (0, 0);
+    var visited = new HashSet<(int, int)> { santa };
+
+    foreach (char c in data)
+    {
+      var (dx, dy) = Directions[c];
+      santa.x += dx;
+      santa.y += dy;
+      visited.Add(santa);
+    }
+    
+    return visited.Count;
+  }
+
+  private static long PartTwo(string data) {
+    (int x, int y) santa = (0, 0);
+    (int x, int y) roboSanta = (0, 0);
+    var visited = new HashSet<(int, int)> { santa };
+
+    foreach ((int idx, char c) in data.Index())
+    {
+      var (dx, dy) = Directions[c];
+      if (idx % 2 == 1)
+      {
+        santa.x += dx;
+        santa.y += dy;
+        visited.Add(santa);
+      }
+      else
+      {
+        roboSanta.x += dx;
+        roboSanta.y += dy;
+        visited.Add(roboSanta);
+      }
+    }
+    
+    return visited.Count;
+  }
+}
---
+++ Day03/readme.md
@@ -0,0 +1,13 @@
+# Advent of Code 2015 #
+## Day 3: Perfectly Spherical Houses in a Vacuum ##
+https://adventofcode.com/2015/day/3
+
+File: sample.txt
+
+Part 1 Result: 2 in 4.6142ms
+Part 2 Result: 11 in 2.1261ms
+
+File: input.txt
+
+Part 1 Result: 2572 in 0.9909ms
+Part 2 Result: 2631 in 1.459ms
